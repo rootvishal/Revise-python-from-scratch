0fa95d74b5d89f1158d58f81d29ab246c72fe5ee
rootvishal <v707jadhav@gmail.com>
2025-08-07 07:22:00 +0530

    pushing the performed opetations

--- File_handaling.py
+++ File_handaling.py
@@ -8,5 +8,19 @@ def Load_the_file(m):
 
 	f.close()
 	print("File closed successfully.")
-Load_the_file("list2.csv")
+
+
+#performing other operations
+
+def Perform_operation_on_load_file(m):
+	f=open(m, "r")
+	print("File opened successfully.")
+	print(f.read(25)) #read 25 the id charcters from the file
+	print(type(f))
+	print(f.name) #print the name of the file	
+	print(f.mode) #print the mode of the file
+
+	f.close()
+	print("File closed successfully.")
+Perform_operation_on_load_file("list2.csv")
 
