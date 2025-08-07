7e75873513bdd0395d2c18f6658c5a8dc36db4b2
rootvishal <v707jadhav@gmail.com>
2025-08-07 07:14:31 +0530

    file handling learning r method complete

--- File_handaling.py
+++ File_handaling.py
@@ -1,1 +1,12 @@
-git 
+print("Learning Python file handling...")
+# This is a simple Python script to demonstrate file handling
+def Load_the_file(m):
+	f=open(m, "r")
+	print("File opened successfully.")
+	print(f.read(25)) #read 25 the id charcters from the file
+	print(type(f))
+
+	f.close()
+	print("File closed successfully.")
+Load_the_file("list2.csv")
+
